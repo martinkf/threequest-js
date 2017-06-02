f771f72f14ea315fbc420ad4ec6d15ef0a3a99c3
MartinKF <martinkf@live.com>
2017-06-01 21:44:10 -0300

    Initial commit

---
+++ threequest_js/Main.js
@@ -0,0 +1,16 @@
+// setup p5
+function setup()
+{
+  // general setup
+  createCanvas(800, 600);
+  frameRate(60);
+}
+
+// draw p5
+function draw()
+{
+  // general setup
+  background(128);
+}
+
+// handling i/o
